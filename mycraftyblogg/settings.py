58543afc1cbba4611f0c118790076386dab0fe7b
volausson <veronicagolausson@gmail.com>
2022-12-15 11:05:21 +0000

    modified settings.py

--- mycraftyblogg/settings.py
+++ mycraftyblogg/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     'default':
     dj_database_url.parse(os.environ.get('DATABASE_URL'))
 
-}
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
